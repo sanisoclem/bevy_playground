f920fc30d1a7fcb1a59b0a3498a6479b6651eac2
Jerahmeel Cosinas <contact@jerahmeelcosinas.net>
2022-06-13 23:45:09 +1000

    why are joints slow in an m1

--- examples/ai.rs
+++ examples/ai.rs
@@ -21,6 +21,7 @@ fn main() {
     .add_plugin(RapierDebugRenderPlugin::default())
     .add_startup_system(spawn_critters)
     .add_startup_system(spawn_arena)
+    .add_startup_system(create_ball_joints)
     .run();
 }
 fn spawn_critters(
@@ -30,7 +31,7 @@ fn spawn_critters(
 ) {
   commands
     .spawn_bundle(PbrBundle {
-      mesh: meshes.add(Mesh::from(shape::Icosphere { radius: 1.0, subdivisions: 20 })),
+      mesh: meshes.add(Mesh::from(shape::Box::new(0.2, 0.2, 1.))),
       material: materials.add(StandardMaterial {
         base_color: Color::LIME_GREEN,
         ..default()
@@ -41,6 +42,69 @@ fn spawn_critters(
     .insert(RigidBody::Dynamic)
     .insert(Collider::ball(1.0))
     .insert(Restitution::coefficient(0.7));
+}
+
+
+fn create_ball_joints(mut commands: Commands) {
+  let num = 15;
+  let rad = 0.4;
+  let shift = 1.0;
+
+  let mut body_entities = Vec::new();
+
+  for k in 0..num {
+      for i in 0..num {
+          let fk = k as f32;
+          let fi = i as f32;
+
+          let rigid_body = if i == 0 && (k % 4 == 0 || k == num - 1) {
+              RigidBody::Fixed
+          } else {
+              RigidBody::Dynamic
+          };
+
+          let child_entity = commands
+              .spawn_bundle(TransformBundle::from(Transform::from_xyz(
+                  fk * shift,
+                  0.0,
+                  fi * shift,
+              )))
+              .insert(rigid_body)
+              .insert(Collider::ball(rad))
+              .id();
+
+          // Vertical joint.
+          if i > 0 {
+              let parent_entity = *body_entities.last().unwrap();
+              let joint = SphericalJointBuilder::new().local_anchor2(Vec3::new(0.0, 0.0, -shift));
+              commands.entity(child_entity).with_children(|children| {
+                  // NOTE: we want to attach multiple impulse joints to this entity, so
+                  //       we need to add the components to children of the entity. Otherwise
+                  //       the second joint component would just overwrite the first one.
+                  children
+                      .spawn()
+                      .insert(ImpulseJoint::new(parent_entity, joint));
+              });
+          }
+
+          // Horizontal joint.
+          if k > 0 {
+              let parent_index = body_entities.len() - num;
+              let parent_entity = body_entities[parent_index];
+              let joint = SphericalJointBuilder::new().local_anchor2(Vec3::new(-shift, 0.0, 0.0));
+              commands.entity(child_entity).with_children(|children| {
+                  // NOTE: we want to attach multiple impulse joints to this entity, so
+                  //       we need to add the components to children of the entity. Otherwise
+                  //       the second joint component would just overwrite the first one.
+                  children
+                      .spawn()
+                      .insert(ImpulseJoint::new(parent_entity, joint));
+              });
+          }
+
+          body_entities.push(child_entity);
+      }
+  }
 }
 
 fn spawn_arena(
